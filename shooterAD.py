994b116c80efd4ed9c3d377e1f44e9745e4b8301
Ansh Desai <anshdesai@anshs-air-3.lan>
2024-02-13 15:20:38 -0500

    YUHHHH

--- shooterAD.py
+++ shooterAD.py
@@ -11,7 +11,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('ShooterAD')
+pygame.display.set_caption('ShooterAnshDesai')
 
 # set framerate
 clock = pygame.time.Clock()
